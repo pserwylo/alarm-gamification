977fa5edfb65dfe78a031429d6ea9ecef25f11da
Peter Serwylo <peter@ivt.com.au>
2013-02-26 17:28:16 +1100

    Basic menu with add alarm and view settings buttons. Doesn't do anything yet.

--- src/com/example/alarm/gamify/activities/AlarmGamification.java
+++ src/com/example/alarm/gamify/activities/AlarmGamification.java
@@ -2,8 +2,10 @@ package com.example.alarm.gamify.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-import java.util.ArrayList;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import com.example.alarm.gamify.R;
 
 public class AlarmGamification extends Activity
 {
@@ -11,6 +13,30 @@ public class AlarmGamification extends Activity
 	public void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
+		super.setContentView( R.layout.main );
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu( Menu menu )
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate( R.menu.main_menu, menu );
+		return true;
+	}
+
+	protected void addAlarm( MenuItem item )
+	{
+		// TODO: Implement this...
+	}
+
+	protected void viewSettings( MenuItem item )
+	{
+		// TODO: Implement this...
+	}
+
+	protected void editAlarm( MenuItem item )
+	{
+		// TODO: Implement this...
 	}
 
 }
